7521f2dcdd1b07aee1964ee23fe47cbe61f7b2d1
Vincent Royer <vroyer@strapdata.com>
2017-04-16 21:55:11 +0200

    Update licence header

--- javassist-maven-plugin-core/src/main/java/com/strapdata/transformer/StringLocaleTransformer.java
+++ javassist-maven-plugin-core/src/main/java/com/strapdata/transformer/StringLocaleTransformer.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2016 Vincent Royer (vroyer@vroyer.org).
- * Contains some code from Elasticsearch (http://www.elastic.co)
+ * Copyright (c) 2016 Strapdata (contact@strapdata.com).
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
